9186f770a352ab4a3b925c1072cce4e567b93df6
mechanikal <251160@edu.p.lodz.pl>
2026-03-13 15:59:05 +0100

    remove setupDefaultDrawerVisibilityManagement

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -53,17 +53,17 @@ public class TestJobs2dPatterns {
 		DriverFeature.updateDriverInfo();
 	}
 
-	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param application Application context.
-	 */
-	private static void setupDefaultDrawerVisibilityManagement(Application application) {
-		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
-		application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
-				new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
-		defaultDrawerWindow.setVisible(true);
-	}
+//	/**
+//	 * Auxiliary routines to enable using Buggy Simulator.
+//	 *
+//	 * @param application Application context.
+//	 */
+//	private static void setupDefaultDrawerVisibilityManagement(Application application) {
+//		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
+//		application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
+//				new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
+//		defaultDrawerWindow.setVisible(false);
+//	}
 
 	/**
 	 * Setup menu for adjusting logging settings.
@@ -91,7 +91,7 @@ public class TestJobs2dPatterns {
 			public void run() {
 				Application app = new Application("2d jobs Visio");
 				DrawerFeature.setupDrawerPlugin(app);
-				//setupDefaultDrawerVisibilityManagement(app);
+//				setupDefaultDrawerVisibilityManagement(app);
 
 				DriverFeature.setupDriverPlugin(app);
 				setupDrivers(app);
