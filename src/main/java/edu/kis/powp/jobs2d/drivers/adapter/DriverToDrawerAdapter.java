c4065d9f3160da23458de58354dd4c7146760c4e
mechanikal <251160@edu.p.lodz.pl>
2026-03-13 13:38:05 +0100

    redirect display to main window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToDrawerAdapter.java
@@ -4,15 +4,17 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class DriverToDrawerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+    private final DrawPanelController drawPanelController;
 
 	public DriverToDrawerAdapter() {
-		super();
+        drawPanelController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,7 +29,7 @@ public class DriverToDrawerAdapter extends DrawPanelController implements Job2dD
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 		setPosition(x,y);
-		drawLine(line);
+        drawPanelController.drawLine(line);
 	}
 
 	@Override
