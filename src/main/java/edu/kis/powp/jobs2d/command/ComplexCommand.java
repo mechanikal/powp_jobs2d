2474188b88b16e220797b7ff683f3b8d2274fa58
mechanikal <251160@edu.p.lodz.pl>
2026-03-28 20:07:16 +0100

    modify complexcommand to enable nesting

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -1,19 +1,11 @@
 package edu.kis.powp.jobs2d.command;
 import java.util.ArrayList;
 
-public class ComplexCommand {
+public class ComplexCommand implements DriverCommand {
     private ArrayList<DriverCommand> driverCommands = new ArrayList<>();
 
     public void addCommand(DriverCommand driverCommand){
         driverCommands.add(driverCommand);
-    }
-
-    public void setCommands(ArrayList<DriverCommand> driverCommands){
-        this.driverCommands = driverCommands;
-    }
-
-    public ArrayList<DriverCommand> getCommands(){
-        return this.driverCommands;
     }
 
     public void execute() {
